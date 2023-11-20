f03d608d283ef22f5be43036ae4378d29e66c520
kimaru355 <emmanuelkimaru355@gmail.com>
2023-11-20 11:07:51 +0300

    added test book

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+const submitButton = document.querySelector('#submitButton');
 const myLibrary = [];
-
 function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -7,3 +7,17 @@ function Book (title, author, pages, read) {
     this.read = read ? 'read': 'not read yet';
     this.info = () => (this.title + ' by ' + this.author + ', ' + this.pages + ' pages. ' + this.read);
 }
+
+
+document.querySelector('#inputBook').addEventListener("submit", (event) => {
+    event.preventDefault;
+    let selectedValue = document.querySelector('input[name="radioGroup"]:checked');
+    console.log(selectedValue);
+    console.log(selectedValue.value);
+});
+
+function addBookToLibrary () {
+    let book1 = Object.create(Book);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
