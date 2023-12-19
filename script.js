f91573a04bc38ac4d8ee8b860862df4369ba8259
kimaru355 <emmanuelkimaru355@gmail.com>
2023-12-19 19:05:41 +0300

    correct typo

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read ? 'read': 'not read yet';
+    this.read = read ? 'read': 'Unread';
     this.info = () => (this.title + ' by ' + this.author + ', ' + this.pages + ' pages. ' + this.read);
 }
 
