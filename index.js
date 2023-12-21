b13fa30c80d97663935af2afa1fee0bdfe67cc35
kimaru355 <emmanuelkimaru355@gmail.com>
2023-12-21 23:45:55 +0300

    library working

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-let inputRead = document.querySelector('#inputRead');	
+let inputBook = document.querySelector('#inputBook');
+let inputTitle = document.querySelector('#inputTitle');
+let inputAuthor = document.querySelector('#inputAuthor');
+let inputPages = document.querySelector('#inputPages');
+let inputRead = document.querySelector('#inputRead');
+let submitBook = document.querySelector('#submitBook');
+let listBooks = document.querySelector('#listBooks');
+let library = [];
+
+let createElement = (element) => {
+    if (element = 'div') {
+        return document.createElement(element);
+    } else if (element = 'p') {
+        return document.createElement(element);
+    } else if (element = 'button') {
+        return document.createElement(element);
+    } else if (element = 'input') {
+        return document.createElement(element);
+    };
+}
 
 inputRead.addEventListener('click', () => {
     if (inputRead.classList.toggle('bg-red-300')) {
@@ -8,4 +27,35 @@ inputRead.addEventListener('click', () => {
         inputRead.textContent = 'Read';
         inputRead.value = true;
     }
+});
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        if (read === true) {
+            this.read = 'Read';
+        } else {
+            this.read = 'Unread';
+        };
+    }
+
+    info () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    }
+}
+
+function addBook(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    library.push(book);
+}
+
+function displayLibrary() {
+    // pass
+}
+
+submitBook.addEventListener('click', () => {
+    addBook(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.value);
+    displayLibrary();
 });
