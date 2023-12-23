715cc7458a14a7ef14a6f9d8233cb87aa2a71ec7
kimaru355 <emmanuelkimaru355@gmail.com>
2023-12-23 12:20:06 +0300

    delete working

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ let listBooks = document.querySelector('#listBooks');
 let library = [];
 
 let createElement = (element) => {
-    if (element = 'div') {
+    if (element === 'div') {
         return document.createElement(element);
-    } else if (element = 'p') {
+    } else if (element === 'p') {
         return document.createElement(element);
-    } else if (element = 'button') {
+    } else if (element === 'button') {
         return document.createElement(element);
-    } else if (element = 'input') {
+    } else if (element === 'input') {
         return document.createElement(element);
     };
 }
@@ -57,20 +57,74 @@ function displayLibrary() {
     for (let i = 0; i < library.length; i++) {
         let book = library[i];
         let div = createElement('div');
-        let p = createElement('p');
+        let info = createElement('p');
         let deleteButton = createElement('button');
         let editButton = createElement('button');
         let controls = createElement('div');
-        p.textContent = book.info();
+
+        info.textContent = book.info();
         deleteButton.textContent = 'Delete';
         editButton.textContent = 'Edit';
         editButton.setAttribute('type', 'button');
         deleteButton.setAttribute('type', 'button');
+        div.setAttribute('id', `book-${i}`);
+
         div.classList.add('flex', 'justify-between', 'items-center', 'border', 'border-gray-300', 'p-4', 'my-4', 'rounded-lg', 'gap-4');
         controls.classList.add('flex', 'gap-4');
-        deleteButton.classList.add('bg-red-300', 'text-white', 'p-2', 'rounded-lg');
-        editButton.classList.add('bg-blue-300', 'text-white', 'p-2', 'rounded-lg');
-        div.appendChild(p);
+        deleteButton.classList.add('bg-red-300', 'hover:bg-red-600', 'text-white', 'p-2', 'rounded-lg');
+        editButton.classList.add('bg-blue-300', 'hover:bg-blue-600', 'text-white', 'p-2', 'rounded-lg');
+
+        editButton.addEventListener('click', () => {
+            let editBook = document.querySelector(`#book-${i}`);
+            let title = createElement('input');
+            let author = createElement('input');
+            let pages = createElement('input');
+            let read = createElement('button');
+            if (editButton.textContent === 'Edit') {
+                editBook.removeChild(info);
+                info = createElement('div');
+
+                title.setAttribute('type', 'text');
+                author.setAttribute('type', 'text');
+                pages.setAttribute('type', 'number');
+                read.setAttribute('type', 'button');
+                title.value = book.title;
+                author.value = book.author;
+                pages.value = book.pages;
+                read.textContent = 'Read';
+                read.addEventListener('click', () => {
+                    if (read.classList.toggle('bg-red-300')) {
+                        read.textContent = 'Unread';
+                    } else {
+                        read.textContent = 'Read';
+                    };
+                });
+
+                info.classList.add('flex', 'gap-2');
+                title.classList.add('w-1/5', 'border', 'border-sky-300', 'pl-2');
+                author.classList.add('w-1/5', 'border', 'border-sky-300', 'pl-2');
+                pages.classList.add('w-1/5', 'border', 'border-sky-300', 'pl-2');
+                read.classList.add('bg-green-300', 'p-2', 'rounded-lg');
+
+                info.appendChild(title);
+                info.appendChild(author);
+                info.appendChild(pages);
+                info.appendChild(read);
+                editBook.insertBefore(info, controls);
+                editButton.textContent = 'Save';
+            } else {
+                editBook.removeChild(info);
+                info = createElement('p');
+                editBook.insertBefore(info, controls);
+                editButton.textContent = 'Edit';
+            };
+        });
+
+        deleteButton.addEventListener('click', () => {
+            library.splice(i, 1);
+            displayLibrary();
+        });
+        div.appendChild(info);
         controls.appendChild(editButton);
         controls.appendChild(deleteButton);
         div.appendChild(controls);
